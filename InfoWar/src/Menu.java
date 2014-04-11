c0416882328a3d122d69d8bab41e0571db63622c
simon <simon@simon-GX60-1AC>
2014-04-11 13:13:36 +0200

    Ajout d'un menu classique Le Main affiche maintenant le menu

---
+++ InfoWar/src/Menu.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Menu {
+
+	/**
+	 * Affiche le menu, lance les fonctions de choix et de redirection.
+	 */
+	public void afficher() {
+		System.out.println("------------------------------");
+		System.out.println("|            MENU            |");
+		System.out.println("------------------------------");
+		System.out.println(" 1. Joueur contre ordinateur ");
+		System.out.println(" 2. Joueur contre joueur  ");
+		System.out.println(" 3. Quitter ");
+		this.actionMenu(this.choixMenu());
+	}
+
+	/**
+	 * Redirectionne l'utilisateur en fonction de sont choix.
+	 * 
+	 * @param choix
+	 */
+	public void actionMenu(int choix) {
+		switch (choix) {
+		case 1:
+			System.out.println("lancement de la partie jce");
+			break;
+		case 2:
+			System.out.println("lancement de la partie jcj");
+			break;
+		case 3:
+			System.out.println("Bye");
+			break;
+
+		default:
+			System.out.println("Veuillez rentrer un choix valide");
+			break;
+		}
+	}
+
+	/**
+	 * Demande à l'utilisateur de faire un choix. Renvoie le choix.
+	 * 
+	 * @return
+	 */
+	public int choixMenu() {
+		return new Scanner(System.in).nextInt();
+	}
+
+}
--- InfoWar/src/Start.java
+++ InfoWar/src/Start.java
@@ -1,8 +1,7 @@
-
 public class Start {
-
 	public static void main(String[] args) {
-		System.out.println("Test de merdique 3 par vincent");
+
+		new Menu().afficher();
+
 	}
-	
 }
