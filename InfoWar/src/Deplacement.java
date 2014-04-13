82b87615d7a936ab3979e02d5e9fbe1b6ed026bf
Gautier <Gautier@Gautier-HP>
2014-04-13 17:25:32 +0200

    Ajout des pertes de point d'energie lors d'un deplacement

--- InfoWar/src/Deplacement.java
+++ InfoWar/src/Deplacement.java
@@ -9,11 +9,17 @@ public class Deplacement extends Action {
 		if (this.getObjectif().getHauteur() < 10
 				&& this.getObjectif().getHauteur() > 0
 				&& this.getObjectif().getLargeur() < 5
-				&& this.getObjectif().getLargeur() > 0) // Verifie que le
+				&& this.getObjectif().getLargeur() > 0) { // Verifie que le
 														// deplacement est dans
 														// le plateau
-			this.getRobot().setCoordonnees(this.getObjectif()); // Change les
+			this.getRobot().setCoordonnees(this.getObjectif());// Change les
 																// coordonnees
 																// du robot
+		this.getRobot().setEnergie(this.getRobot().getEnergie() - this.getRobot().getCoutDep());
+			
+		}
+		else {
+			this.getRobot().setEnergie(this.getRobot().getEnergie() - this.getRobot().getCoutDep());
+		}
 	}
 }
