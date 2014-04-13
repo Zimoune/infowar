5ce1da6d15458608090b3caf4559315653773363
Gautier <Gautier@Gautier-HP>
2014-04-13 22:01:40 +0200

    Ajout de Commentaire

--- InfoWar/src/Attaque.java
+++ InfoWar/src/Attaque.java
@@ -7,35 +7,34 @@ public class Attaque extends Action {
 	@Override
 	public void agit() {
 		// TODO Auto-generated method stub
-		if(this.getRobot() instanceof Tireur){
-		if (this.getRobot().peutTirer()
-				&& this.getRobot().getVue().plateau.estRobot(this.getObjectif()
+		if(this.getRobot() instanceof Tireur){ // Le robot est un tireur
+		if (this.getRobot().peutTirer() //Verifie si le robot peut tirer et si l'objectif est correcte
 						.getLargeur(), this.getObjectif().getHauteur())){
 			this.getRobot().getVue().plateau.tableau[this.getObjectif()
 					.getLargeur()][this.getObjectif().getHauteur()]
-					.getContenu().subitTir();
-			this.getRobot().setEnergie(this.getRobot().getEnergie()-this.getRobot().getCoutAction());
+					.getContenu().subitTir(); // Fait des degat au robot toucher
+			this.getRobot().setEnergie(this.getRobot().getEnergie()-this.getRobot().getCoutAction()); // Enleve l'energie du tireur
 		}
-		else if (this.getRobot().getEnergie() < this.getRobot().getCoutAction()) {
+		else if (this.getRobot().getEnergie() < this.getRobot().getCoutAction()) { // Si le robot ne peut pas tirer
 			System.out.println("Pas assez d'energie");
 	}
 		else {
-			System.out.println("Vous ne pouvez pas tirer ici");
+			System.out.println("Vous ne pouvez pas tirer ici"); // Si la cible n'est pas possible
 		}
 	}
 		
-		if (this.getRobot() instanceof Piegeur){
-			if(this.getRobot().getEnergie() > this.getRobot().getCoutAction()){
+		if (this.getRobot() instanceof Piegeur){ // Le robot est un piegeur
+			if(this.getRobot().getEnergie() > this.getRobot().getCoutAction()){ // Verifie si il peut poser une mine
 				if(!this.getRobot().getVue().estMine(this.getObjectif()) && this.getRobot().getVue().plateau.estRobot(this.getObjectif().getLargeur(),this.getObjectif().getHauteur()))
-						{
-							this.getRobot().getVue().setMine(getObjectif(), this.getRobot().getEquipe());
+						{ //Verifie si la case est libre
+							this.getRobot().getVue().setMine(getObjectif(), this.getRobot().getEquipe()); // pose la mine
 				}
 				else {
-					System.out.println("Vous ne pouvez pas posez de mine ici");
+					System.out.println("Vous ne pouvez pas posez de mine ici"); // Cas si la case n'est pas libre
 				}
 			}
 			else {
-				System.out.println("Vous n'avez pas assez d'energie");
+				System.out.println("Vous n'avez pas assez d'energie"); // Cas si le robot ne peut pas poser de mine
 			}
 		}
 	}
