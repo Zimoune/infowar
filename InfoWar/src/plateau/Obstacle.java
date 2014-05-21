92f46bb20c50e16d38eedd59547badb6e3e296be
Rémy <Rmy@Rmy-HP>
2014-05-21 20:47:40 +0200

    suite

---
+++ InfoWar/src/plateau/Obstacle.java
@@ -0,0 +1,44 @@
+package plateau;
+
+import robot.Robot;
+
+public class Obstacle extends Cellule{
+
+	public Obstacle(int x, int y) {
+		super(x, y);
+		this.image = "O";
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void deplaceSur(Robot r) {
+		System.out.println("Impossible de se deplacer ici");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ajout(int equipe) {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void videCase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		return this.toString();
+	}
+
+	@Override
+	public void ajoutObstacle() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
