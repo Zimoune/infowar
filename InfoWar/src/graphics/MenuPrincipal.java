0bb9b4ca575b3d07b937d52470c4c2aa9900bcc2
vincent <vincent@vincent-K73SJ>
2014-05-26 20:42:39 +0200

    Menu et Options v2

--- InfoWar/src/graphics/MenuPrincipal.java
+++ InfoWar/src/graphics/MenuPrincipal.java
@@ -1,13 +1,97 @@
 package graphics;
 
-public class MenuPrincipal {
 
-	/**
-	 * @param args
-	 */
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MenuPrincipal extends JFrame implements ActionListener {
+
+	JButton jcj;
+	JButton jco;
+	JButton options;
+	JButton quitter;
+	JPanel panelButton;
+	JLabel titre;
+	JPanel menu;
+
+	public MenuPrincipal() {
+		jcj = new JButton("Joueur contre Joueur");
+		jco = new JButton("Joueur contre Ordinateur");
+		options = new JButton("Options");
+		quitter = new JButton("Quitter");
+		panelButton = new JPanel();
+		titre = new JLabel("VirtualWar");
+		menu = new JPanel();
+
+		titre.setFont(new Font("Serif", Font.BOLD, 48));
+
+		menu.setLayout(new BoxLayout(menu, BoxLayout.PAGE_AXIS));
+
+		panelButton.setLayout(new GridLayout(4, 3, 50, 50));
+
+		jcj.addActionListener(this);
+		jco.addActionListener(this);
+		options.addActionListener(this);
+		quitter.addActionListener(this);
+
+		menu.add(titre);
+		menu.add(Box.createRigidArea(new Dimension(0, 30)));
+
+		panelButton.add(Box.createRigidArea(new Dimension(0, 0)));
+		panelButton.add(jcj);
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		panelButton.add(jco);
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		panelButton.add(options);
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		panelButton.add(quitter);
+		panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+
+		menu.add(Box.createRigidArea(new Dimension(0, 60)));
+		menu.add(panelButton);
+		menu.add(Box.createRigidArea(new Dimension(0, 100)));
+
+		panelButton.setAlignmentX(CENTER_ALIGNMENT);
+		jcj.setAlignmentX(CENTER_ALIGNMENT);
+		jco.setAlignmentX(CENTER_ALIGNMENT);
+		options.setAlignmentX(CENTER_ALIGNMENT);
+		quitter.setAlignmentX(CENTER_ALIGNMENT);
+		titre.setAlignmentX(CENTER_ALIGNMENT);
+
+		this.setContentPane(menu);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Stub de la méthode généré automatiquement
+		if (e.getSource() == options) {
+			this.setContentPane(new Options());
+			this.revalidate();
+		} else if (e.getSource() == quitter) {
+			System.exit(0);
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Stub de la méthode généré automatiquement
-
+		JFrame f = new MenuPrincipal();
+		f.setTitle("VirtualWar");
+		f.pack();
+		f.setDefaultLookAndFeelDecorated(false);
+		f.setExtendedState(MAXIMIZED_BOTH);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 }
--- InfoWar/src/graphics/Options.java
+++ InfoWar/src/graphics/Options.java
@@ -1,59 +1,119 @@
 package graphics;
 
-import java.awt.CardLayout;
+
+
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
-public class Options extends JPanel implements ActionListener {
+public class Options extends JPanel implements ActionListener, KeyListener {
 
-	JLabel titre = new JLabel("Options");
-	JLabel nbRobot = new JLabel("Nombre de robots par équipe :");
-	JLabel perCentObs = new JLabel("Pourcentage d'obstacle :");
-	JLabel taillePlateau = new JLabel("Taille du plateau :");
-	JLabel niveauDifficulte = new JLabel("Niveau de difficulté :");
-	JLabel musique = new JLabel("Musique");
-	JSpinner choixNbRobot = new JSpinner();
-	JSpinner choixObs = new JSpinner();
-	JTextField hauteur = new JTextField("15", 2);
-	JTextField largeur = new JTextField("12",2);
-	JSpinner choixDifficutle = new JSpinner();
-	JSlider volume = new JSlider(0, 100);
-	JPanel core = new JPanel();
-	JPanel taille = new JPanel();
-	
-	
-	public Options(){
-		this.setLayout(null);
-		core.setLayout(new GridLayout(5,2));
-		
+	JLabel titre;
+	JLabel nbRobot;
+	JLabel perCentObs;
+	JLabel taillePlateau;
+	JLabel niveauDifficulte;
+	JLabel musique;
+	JSpinner choixNbRobot;
+	JSpinner choixObs;
+	JSpinner hauteur;
+	JSpinner largeur;
+	JSpinner choixDifficutle;
+	JSlider volume;
+	JPanel core;
+	JPanel taille;
+	JButton retour;
+
+	public Options() {
+
+		taille = new JPanel();
+		core = new JPanel();
+		volume = new JSlider(0, 100);
+		choixDifficutle = new JSpinner(new SpinnerNumberModel(0, 0, 5, 1));
+		largeur = new JSpinner(new SpinnerNumberModel(15, 5, 20, 1));
+		hauteur = new JSpinner(new SpinnerNumberModel(12, 5, 20, 1));
+		choixObs = new JSpinner(new SpinnerNumberModel(20, 20, 80, 10));
+		choixNbRobot = new JSpinner(new SpinnerNumberModel(1, 1, 5, 1));
+		musique = new JLabel("Musique");
+		niveauDifficulte = new JLabel("Niveau de difficulté :");
+		taillePlateau = new JLabel("Taille du plateau :");
+		perCentObs = new JLabel("Pourcentage d'obstacle :");
+		nbRobot = new JLabel("Nombre de robots par équipe :");
+		titre = new JLabel("Options");
+		retour = new JButton("Retour");
+
+		titre.setFont(new Font("Serif", Font.BOLD, 48));
+
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		core.setLayout(new GridLayout(5, 4, 200, 100));
+
+		largeur.addKeyListener(this);
+
 		taille.add(hauteur);
 		taille.add(largeur);
-		
+
+		retour.addActionListener(this);
+
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
 		core.add(nbRobot);
 		core.add(choixNbRobot);
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
 		core.add(perCentObs);
 		core.add(choixObs);
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
 		core.add(taillePlateau);
 		core.add(taille);
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
 		core.add(niveauDifficulte);
 		core.add(choixDifficutle);
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
+		core.add(Box.createRigidArea(new Dimension(30, 0)));
 		core.add(musique);
 		core.add(volume);
-		
-		this.add(titre);
-		titre.setBounds(200, 10, 100, 10);
-		this.add(core);
-		core.setBounds(100,100,300,300);
-	}
 
+		this.add(titre);
+		titre.setAlignmentX(CENTER_ALIGNMENT);
+		this.add(Box.createRigidArea(new Dimension(0, 100)));
+		this.add(core);
+		core.setAlignmentX(CENTER_ALIGNMENT);
+		this.add(retour);
+		retour.setAlignmentX(CENTER_ALIGNMENT);
+		this.add(Box.createRigidArea(new Dimension(0, 100)));
+	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Stub de la méthode généré automatiquement
-		
+		// Sauvegarde des changements
+		JOptionPane jop = new JOptionPane();
+		jop.showMessageDialog(null, "Message d'erreur", "Erreur",
+				JOptionPane.ERROR_MESSAGE);
 	}
-	
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Stub de la méthode généré automatiquement
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Stub de la méthode généré automatiquement
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Stub de la méthode généré automatiquement
+
+	}
+
 }
