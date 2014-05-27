58435a4ec86be7b7081e6d73b6724ec83e35887b
vincent <vincent@vincent-K73SJ>
2014-05-27 22:16:49 +0200

    Ajout package

---
+++ InfoWar/src/outilsGraphic/MyJButton.java
@@ -0,0 +1,25 @@
+package outilsGraphic;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+public class MyJButton extends JButton {
+
+	public MyJButton() {
+		super();
+		// TODO Stub du constructeur généré automatiquement
+	}
+
+	public MyJButton(String text, Icon icon) {
+		super(text, icon);
+		// TODO Stub du constructeur généré automatiquement
+	}
+
+	public MyJButton(String text) {
+		super(text);
+		// TODO Stub du constructeur généré automatiquement
+	}
+
+	
+
+}
