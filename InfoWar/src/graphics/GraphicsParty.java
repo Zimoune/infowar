4677d41388ca2e6209874f42a905a82d787039aa
Zimoune <lepallac.simon@gmail.com>
2014-05-27 22:14:37 +0200

    maj MenuPanel et nouvelle Classe GraphicsParty

---
+++ InfoWar/src/graphics/GraphicsParty.java
@@ -0,0 +1,16 @@
+package graphics;
+
+import javax.swing.JPanel;
+
+public class GraphicsParty extends JPanel{
+
+	public GraphicsParty(int typeDePartie){
+		if(typeDePartie == 1 ){
+			//On lance la partie JCJ
+		}
+		else{
+			//On lance la partie IAvsIA
+		}
+	}
+	
+}
