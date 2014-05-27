38f36254a9740eeab6871d88c67fbe0b91075e77
vincent <vincent@vincent-K73SJ>
2014-05-27 21:58:14 +0200

    MenuPanel

--- InfoWar/src/graphics/MenuPanel.java
+++ InfoWar/src/graphics/MenuPanel.java
@@ -21,32 +21,38 @@ public class MenuPanel extends JPanel implements ActionListener{
 	private JButton options = new JButton("Options");
 	private JButton quitter = new JButton("Quitter");
 	private JFrame frame;
+	JPanel panelButton = new JPanel();
 	
 	public MenuPanel(JFrame frame){
 		this.frame = frame;
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.titre.setFont(new Font("Serif", Font.BOLD, 48));
 		this.add(titre);
-		this.add(Box.createRigidArea(new Dimension(0, 30)));
+		this.add(Box.createRigidArea(new Dimension(30, 0)));
+		/*this.add(Box.createRigidArea(new Dimension(0, 30)));
 		this.add(Box.createRigidArea(new Dimension(0, 60)));
-		this.add(Box.createRigidArea(new Dimension(0, 100)));
-		this.setLayout(new GridLayout(4, 3, 50, 50));
+		this.add(Box.createRigidArea(new Dimension(0, 100)));*/
+		
 		this.jcj.addActionListener(this);
 		this.jco.addActionListener(this);
 		this.options.addActionListener(this);
 		this.quitter.addActionListener(this);
-		this.add(Box.createRigidArea(new Dimension(0, 0)));
-		this.add(jcj);
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
-		this.add(jco);
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
-		this.add(options);
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
-		this.add(quitter);
-		this.add(Box.createRigidArea(new Dimension(30, 0)));
+		
+		this.panelButton.setLayout(new GridLayout(4, 3, 50, 50));
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(jcj);
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(jco);
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(options);
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		this.panelButton.add(quitter);
+		this.panelButton.add(Box.createRigidArea(new Dimension(30, 0)));
+		
+		this.add(panelButton);
 		this.setAlignmentX(CENTER_ALIGNMENT);
 		this.jcj.setAlignmentX(CENTER_ALIGNMENT);
 		this.jco.setAlignmentX(CENTER_ALIGNMENT);
