cd7d088f19204d3251123f8be52839789de8958e
vincent <vincent@vincent-K73SJ>
2014-05-28 18:35:47 +0200

    Deplacement

--- InfoWar/src/action/Deplacement.java
+++ InfoWar/src/action/Deplacement.java
@@ -21,16 +21,24 @@ public class Deplacement extends Action {
 	public void agit() {
 		//On verifie que la direction est differente de null
 		if (this.getDirection() != null) {
+			if(this.getRobot().getType().equalsIgnoreCase("c")){
+				if(this.getRobot().getVue().getContenu(getObjectif()) != null && !this.getRobot().getVue().estObstacle(getObjectif())){
+					this.getRobot().getVue().deplaceSur(this.getObjectif(), this.getRobot());
+					if(this.getRobot().getVue().getContenu(new Coordonnees(this.getObjectif().getLargeur()+this.getDirection().getLargeur(),this.getObjectif().getHauteur()+this.getDirection().getHauteur())) != null && !this.getRobot().getVue().estObstacle(new Coordonnees(this.getObjectif().getLargeur()+this.getDirection().getLargeur(),this.getObjectif().getHauteur()+this.getDirection().getHauteur()))){
+						this.getRobot().getVue().deplaceSur(new Coordonnees(this.getObjectif().getLargeur()+this.getDirection().getLargeur(),this.getObjectif().getHauteur()+this.getDirection().getHauteur()), this.getRobot());
+					}
+				}
+			}
 			
 			//On verifie que la case choisi est bien vide
-			if (this.getRobot().getVue().getContenu(this.getObjectif()) == null) {
+			if (this.getRobot().getVue().getContenu(this.getObjectif()) == null && !this.getRobot().getVue().estObstacle(this.getObjectif())) {
 				//On deplace le robot sur cette case
 				this.getRobot().getVue().deplaceSur(this.getObjectif(), this.getRobot());
 			}
 			
 			//Que la case soit vide ou pas on enleve l'energie au robot
-			this.getRobot().setEnergie(this.getRobot().getEnergie()-1);
-			System.out.println("Vous avez perdu 1 point d'energie");
+			this.getRobot().setEnergie(this.getRobot().getEnergie()-this.getRobot().getCoutDep());
+			System.out.println("Vous avez perdu " + this.getRobot().getCoutDep() + " point d'energie");
 		}		
 	}
 }
